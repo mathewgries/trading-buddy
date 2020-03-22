de8c536e2db6a84049d03802c15b83ef2ee9c882
Gries, Matt <Matt.Gries@freedompay.com>
2020-03-22 12:20:53 -0400

    Adding a Netlify Build Script

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import GrowthCalculator from './components/GrowthCalculator'
 import TradingBuddy from './components/TradingBuddy'
+import NotFound from './components/NotFound'
 
 export default function Router() {
     return (
@@ -10,6 +11,7 @@ export default function Router() {
             <Route exact path='/' component={Dashboard} />
             <Route exact path='/growthCalculator' component={GrowthCalculator}/>
             <Route exact path='/tradingBuddy' component={TradingBuddy}/>
+            <Route component={NotFound}/>   
         </Switch>
     )
 }
