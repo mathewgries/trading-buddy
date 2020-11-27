e05f5de2731b919f5e131669b7cb4559125bce97
Gries, Matt <Matt.Gries@freedompay.com>
2020-11-26 19:46:09 -0500

    changed label

--- src/probabilityCalculator/components/ShowData.js
+++ src/probabilityCalculator/components/ShowData.js
@@ -84,10 +84,9 @@ class ShowData extends Component {
         const { gainPercent } = this.state
         let matches = []
         data.forEach((value, index, elements) => {
-            if (index < elements.length - 1) {
+            if (index > 5 && index < elements.length - 1) {
                 const current = elements[index]
                 const next = elements[index + 1]
-
                 const close = parseFloat(current.c)
                 const hod = parseFloat(next.h)
                 const perCalc = ((hod - close) / close) * 100
@@ -96,17 +95,50 @@ class ShowData extends Component {
                 const closeData = new Date(current.t).toLocaleString()
                 const hodDate = new Date(next.t).toLocaleString()
                 const priceChange = next.c - current.c
+                const prevVolume = [
+                    {
+                        vol: elements[index - 5].v,
+                        volDiff: next.v - elements[index - 5].v,
+                        priceDiff: next.h - elements[index - 5].h,
+                        date: new Date(elements[index - 5].t).toLocaleString()
+                    },
+                    {
+                        vol: elements[index - 4].v,
+                        volDiff: next.v - elements[index - 4].v,
+                        priceDiff: next.h - elements[index - 4].h,
+                        date: new Date(elements[index - 4].t).toLocaleString()
+                    },
+                    {
+                        vol: elements[index - 3].v,
+                        volDiff: next.v - elements[index - 3].v,
+                        priceDiff: next.h - elements[index - 3].h,
+                        date: new Date(elements[index - 3].t).toLocaleString()
+                    },
+                    {
+                        vol: elements[index - 2].v,
+                        volDiff: next.v - elements[index - 2].v,
+                        priceDiff: next.h - elements[index - 2].h,
+                        date: new Date(elements[index - 2].t).toLocaleString()
+                    },
+                    {
+                        vol: elements[index - 1].v,
+                        volDiff: next.v - elements[index - 1].v,
+                        priceDiff: next.h - elements[index - 1].h,
+                        date: new Date(elements[index - 1].t).toLocaleString()
+                    }
+                ]
 
                 if (perCalc >= gainPercent) {
-                    matches.push({ 
-                        close: current, 
+                    matches.push({
+                        close: current,
                         hod: next,
                         compare: {
                             closeData,
                             hodDate,
                             priceChange,
                             volDiff,
-                            vwDiff
+                            vwDiff,
+                            prevVolume
                         }
                     })
                 }
@@ -154,7 +186,7 @@ class ShowData extends Component {
                         </select>
                     </div>
                     <div className='form-group'>
-                        <label>Aggregate Span</label>
+                        <label>Percent Gain</label>
                         <input
                             className='form-control'
                             type='number'
@@ -198,7 +230,12 @@ class ShowData extends Component {
                         {aggDataToggle ? <pre>{JSON.stringify(aggData, null, 2)}</pre> : null}
                     </div>
                     <div>
-                        {moversToggle ? <pre>{JSON.stringify(movers, null, 2)}</pre> : null}
+                        <div>
+                            <p>Match Count: {movers.length}</p>
+                        </div>
+                        <div>
+                            {moversToggle ? <pre>{JSON.stringify(movers, null, 2)}</pre> : null}
+                        </div>
                     </div>
                 </div>
             </div>
